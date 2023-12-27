84e06a266993a22d47c4f8ffa60514adc4b38c23
TinoMaster <oscarmarcos2673@gmail.com>
2023-12-27 12:25:30 +0100

    last

--- components/helpers/TitleTypeMenu.tsx
+++ components/helpers/TitleTypeMenu.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 
 export const TitleTypeMenu = () => {
   const pathname = usePathname();
-  const title = cutPathnameByPiece(pathname)[2];
+  const title = cutPathnameByPiece(pathname, 2);
   return (
     <article className="text-center pb-5">
       <h2 className="uppercase text-slate-200 font-serif text-3xl italic">
