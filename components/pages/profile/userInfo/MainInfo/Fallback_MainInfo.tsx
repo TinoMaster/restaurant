6f286c970eae68ab3851c8a4f0b2cc137f674594
TinoMaster <oscarmarcos2673@gmail.com>
2024-01-03 22:57:31 +0100

    ok

---
+++ components/pages/profile/userInfo/MainInfo/Fallback_MainInfo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Fallback_MainInfo = () => {
+  return (
+    <div className="bg-white/5 grid grid-cols-4 min-h-[40vh] w-full justify-center items-center rounded-lg mb-4 gap-2 animate-pulse pb-4">
+      <div className="p-10 col-span-4 lg:col-span-1 m-auto">
+        <div className="w-44 h-44 rounded-full bg-white/20"></div>
+      </div>
+      <div className="col-span-4 lg:col-span-3 grid md:grid-cols-2 gap-4 px-5">
+        <div className="w-full h-10 bg-white/20 col-span-1 rounded-lg"></div>
+        <div className="w-full h-10 bg-white/20 col-span-1 rounded-lg"></div>
+        <div className="w-full h-10 bg-white/20 col-span-1 rounded-lg"></div>
+        <div className="w-full h-10 bg-white/20 col-span-1 rounded-lg"></div>
+      </div>
+    </div>
+  );
+};
--- components/pages/profile/userInfo/MainInfo/MainInfo.tsx
+++ components/pages/profile/userInfo/MainInfo/MainInfo.tsx
@@ -5,9 +5,9 @@ import { useMainInfo } from "@/hooks/pages/profile/useMainInfo";
 import { useDialogs } from "@/hooks/useDialogs";
 import { useAppSelector } from "@/redux/hooks";
 import { Dialogs_Render } from "./Dialogs_Render";
+import { Fallback_MainInfo } from "./Fallback_MainInfo";
 import { ImageBlock } from "./ImageBlock";
 import { InputEditable } from "./InputEditable";
-import LoadingSkeletonProfile from "@/app/(pages)/(out)/profile/user_info/loading";
 
 export const MainInfo = () => {
   const { emailVerified, phoneVerified, _id } = useAppSelector(
@@ -26,67 +26,66 @@ export const MainInfo = () => {
   const { openDialog } = useDialogs();
 
   if (!_id) {
-    return <LoadingSkeletonProfile />;
-  }
+    return <Fallback_MainInfo />;
+  } else
+    return (
+      <>
+        <Dialogs_Render />
+        <div className="grid grid-cols-4 justify-center items-center py-4 gap-10 md:gap-0">
+          {/* Caja imagen */}
+          <div className="flex flex-col gap-2 justify-center items-center col-span-4 md:col-span-1">
+            <ImageBlock
+              handleChangeImage={handleChangeImage}
+              imagePreview={imagePreview}
+              onChangeImage={onChangeImage}
+            />
+          </div>
+          {/* Caja información */}
+          <div className="col-span-4 md:col-span-3">
+            <h3 className="text-3xl mb-5">User Info</h3>
+            <form
+              onSubmit={handleSubmitUpdateUserInfo}
+              className="grid grid-cols-1 lg:grid-cols-2 gap-4"
+            >
+              {userInfoProfilePageInputs.map((inp, idx) => (
+                <InputEditable
+                  handlerInfoToEdit={handlerInfoToEdit}
+                  userInfoToEdit={userInfoToEdit}
+                  key={idx}
+                  inp={inp}
+                  emailVerified={emailVerified}
+                  phoneVerified={phoneVerified}
+                  openDialog={openDialog}
+                />
+              ))}
+              {/* Buttons box */}
+              <div className="col-span-2 lg:col-span-1 flex justify-end">
+                <div className="flex gap-2 items-end">
+                  <>
+                    <Btn_profile
+                      type="submit"
+                      name="Edit"
+                      disabled={!editonMode}
+                      title={editonMode ? "Edit" : "You must do any change"}
+                    />
 
-  return (
-    <>
-      <Dialogs_Render />
-      <div className="grid grid-cols-4 justify-center items-center py-4 gap-10 md:gap-0">
-        {/* Caja imagen */}
-        <div className="flex flex-col gap-2 justify-center items-center col-span-4 md:col-span-1">
-          <ImageBlock
-            handleChangeImage={handleChangeImage}
-            imagePreview={imagePreview}
-            onChangeImage={onChangeImage}
-          />
-        </div>
-        {/* Caja información */}
-        <div className="col-span-4 md:col-span-3">
-          <h3 className="text-3xl mb-5">User Info</h3>
-          <form
-            onSubmit={handleSubmitUpdateUserInfo}
-            className="grid grid-cols-1 lg:grid-cols-2 gap-4"
-          >
-            {userInfoProfilePageInputs.map((inp, idx) => (
-              <InputEditable
-                handlerInfoToEdit={handlerInfoToEdit}
-                userInfoToEdit={userInfoToEdit}
-                key={idx}
-                inp={inp}
-                emailVerified={emailVerified}
-                phoneVerified={phoneVerified}
-                openDialog={openDialog}
-              />
-            ))}
-            {/* Buttons box */}
-            <div className="col-span-2 lg:col-span-1 flex justify-end">
-              <div className="flex gap-2 items-end">
-                <>
-                  <Btn_profile
-                    type="submit"
-                    name="Edit"
-                    disabled={!editonMode}
-                    title={editonMode ? "Edit" : "You must do any change"}
-                  />
-
-                  <Btn_profile
-                    type="button"
-                    trigger={() => setEditonMode(false)}
-                    name="Change password"
-                    disabled={!emailVerified}
-                    title={
-                      emailVerified
-                        ? "Change password"
-                        : "You must verify your email"
-                    }
-                  />
-                </>
+                    <Btn_profile
+                      type="button"
+                      trigger={() => setEditonMode(false)}
+                      name="Change password"
+                      disabled={!emailVerified}
+                      title={
+                        emailVerified
+                          ? "Change password"
+                          : "You must verify your email"
+                      }
+                    />
+                  </>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 };
