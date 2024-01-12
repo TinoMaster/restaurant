562b016b775e59cbd37294507e14b2a8b1a128e8
TinoMaster <oscarmarcos2673@gmail.com>
2024-01-12 18:31:25 +0100

    ok

--- components/pages/profile/address/CardAddress.tsx
+++ components/pages/profile/address/CardAddress.tsx
@@ -25,7 +25,7 @@ export const CardAddress = ({
                pathname: '/profile/address',
                query: { dialog: DIALOG_EDIT_ADDRESS, id: JSON.stringify(_id) },
             }}
-            className="bg-white text-xs lg:text-sm dark:bg-gray-100/5 shadow-lg rounded-lg p-6 transition duration-300 hover:scale-105 hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100/10"
+            className="text-xs lg:text-sm bg-gray-100/5 shadow-lg rounded-lg p-6 transition duration-300 hover:scale-105 hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100/10"
          >
             <h4 className="text-base lg:text-xl font-semibold">{name}</h4>
             <p className="text-gray-500">{city}</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,59 +1,60 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            'gradient-conic':
+               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+         },
+         colors: {
+            primary: '#D6B57D',
+            secondary: '#ec4749',
+            darkMode: '#151618',
+            lightDarkMode: '#1B1C1E',
+            darkContrast: '#161e2d',
+            primaryPal: {
+               '50': '#fbf9f1',
+               '100': '#f5f0df',
+               '200': '#ebdebd',
+               '300': '#dec693',
+               '400': '#d6b57d',
+               '500': '#c5924a',
+               '600': '#b77d3f',
+               '700': '#986436',
+               '800': '#7b5131',
+               '900': '#64432a',
+               '950': '#352215',
+            },
+            secondaryPal: {
+               '50': '#fef2f2',
+               '100': '#fde3e3',
+               '200': '#fdcbcc',
+               '300': '#faa7a8',
+               '400': '#f57476',
+               '500': '#ec4749',
+               '600': '#d82a2c',
+               '700': '#b61f21',
+               '800': '#971d1f',
+               '900': '#611819',
+               '950': '#440b0c',
+            },
+         },
+         maxWidth: {
+            '1080p': '1920px',
+            '720p': '1280px',
+         },
+         brightness: {
+            '25': '15%',
+         },
       },
-      colors: {
-        primary: "#D6B57D",
-        secondary: "#ec4749",
-        darkMode: "#151618",
-        lightDarkMode: "#1B1C1E",
-        primaryPal: {
-          "50": "#fbf9f1",
-          "100": "#f5f0df",
-          "200": "#ebdebd",
-          "300": "#dec693",
-          "400": "#d6b57d",
-          "500": "#c5924a",
-          "600": "#b77d3f",
-          "700": "#986436",
-          "800": "#7b5131",
-          "900": "#64432a",
-          "950": "#352215",
-        },
-        secondaryPal: {
-          "50": "#fef2f2",
-          "100": "#fde3e3",
-          "200": "#fdcbcc",
-          "300": "#faa7a8",
-          "400": "#f57476",
-          "500": "#ec4749",
-          "600": "#d82a2c",
-          "700": "#b61f21",
-          "800": "#971d1f",
-          "900": "#611819",
-          "950": "#440b0c",
-        },
-      },
-      maxWidth: {
-        "1080p": "1920px",
-        "720p": "1280px",
-      },
-      brightness:{
-        "25": "15%",
-      }
-    },
-  },
-  plugins: [],
-};
-export default config;
+   },
+   plugins: [],
+}
+export default config
