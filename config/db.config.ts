5d2d7a2c44bb7854ab5eb458318fd2582bf8af84
TinoMaster <oscarmarcos2673@gmail.com>
2024-02-14 14:43:09 +0100

    ok

--- config/db.config.ts
+++ config/db.config.ts
@@ -1,5 +1,5 @@
 export const db_config = {
-   host: 'mongodb://localhost:27017',
-   database: 'restaurant',
-   URI: 'mongodb://localhost:27017/restaurant',
+   host: process.env.MONGO_HOST,
+   database: process.env.MONGO_DB,
+   URI: process.env.MONGODB_URI,
 }
