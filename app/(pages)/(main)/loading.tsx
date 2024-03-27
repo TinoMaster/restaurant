d848febfc5f26ef86eb8b53a6fa60758a3bc8924
TinoMaster <oscarmarcos2673@gmail.com>
2024-03-27 13:18:07 +0100

    ok

--- app/(pages)/(main)/loading.tsx
+++ app/(pages)/(main)/loading.tsx
@@ -1,15 +1,17 @@
+import { LinksSectionFallback } from "@/components/pages/home/LinksSectionFallback";
+
 export default function LoadingSkeletonPages() {
    return (
-      <div className="flex flex-col items-center w-screen h-screen bg-darkMode/5 animate-pulse">
+      <div className="flex flex-col items-center w-screen h-screen animate-pulse">
          {/* Skeleton del banner que ocupa el 60% de la altura visible (vh) */}
-         <div className="bg-darkMode/20 h-[60vh] w-full mb-4 flex flex-col gap-2 justify-center items-center animate-pulse">
+         <div className="container h-[60vh] mb-4 flex flex-col gap-2 justify-center items-center">
             <p className="max-w-720p w-[200px] h-5 rounded-full bg-white/20"></p>
-            <p className="max-w-720p w-[500px] h-10 rounded-full bg-white/20"></p>
+            <p className="max-w-720p w-full mx-4 h-10 rounded-full bg-white/20"></p>
          </div>
 
          {/* Skeleton del contenido debajo del banner */}
          <div className="rounded-tr-[60px] relative -translate-y-8 bg-lightDarkMode w-full h-[40vh] max-h-[600px]">
-            {/* Contenido del esqueleto (puede estar vacío ya que es solo un esqueleto) */}
+            <LinksSectionFallback />
          </div>
       </div>
    )
--- components/ui/globals/MenuInfinite.tsx
+++ components/ui/globals/MenuInfinite.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { convertPathWithSpacesReverse, cutPathnameByPiece } from '@/libs/utils'
+import { prenotazione } from '@/utils/images'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react'
@@ -34,14 +36,21 @@ export const MenuInfinite = ({
       <motion.header
          ref={element}
          whileTap={{ cursor: 'grabbing' }}
-         className="flex bg-lightDarkMode overflow-hidden relative py-4 mb-4 lg:mb-10 px-2"
+         className="flex w-full bg-lightDarkMode overflow-hidden relative py-4 mb-4 lg:mb-10 px-2"
       >
+         <Image
+            src={prenotazione}
+            alt="logo"
+            width={100}
+            height={100}
+            className="absolute top-0 left-0 w-full h-full object-cover brightness-25 z-0"
+         />
          <motion.nav
             drag="x"
             dragConstraints={{ right: 0, left: -width }}
             className={`${position === 'center' && 'md:justify-center'} ${
                position === 'right' && 'md:justify-end'
-            } flex w-max gap-4 lg:gap-7 cursor-grab min-w-full`}
+            } flex w-max gap-4 lg:gap-7 cursor-grab mx-auto`}
          >
             {links.map(({ href, title }) => (
                <Link
@@ -50,14 +59,14 @@ export const MenuInfinite = ({
                   scroll={false}
                   className={` ${
                      convertPathWithSpacesReverse(path) === href
-                        ? 'text-gray-800'
+                        ? 'text-gray-50'
                         : 'hover:text-white'
-                  } uppercase text-xs sm:text-sm flex justify-center items-center gap-1 bg-white/5 p-2 rounded-lg relative`}
+                  } uppercase text-sm sm:text-base flex justify-center items-center gap-1 bg-white/5 p-2 rounded-lg relative`}
                >
                   {convertPathWithSpacesReverse(path) === href && (
                      <motion.div
                         layoutId="active2"
-                        className="absolute bg-white w-full px-2 h-full rounded-md"
+                        className="absolute bg-pri-600/70 w-full px-2 h-full rounded-md"
                      ></motion.div>
                   )}
                   <span className="leading-none z-10 w-max flex justify-center items-center">
