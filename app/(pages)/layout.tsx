d2356c6a9ec62618d9c9ae758fd68a94be8d1f28
TinoMaster <oscarmarcos2673@gmail.com>
2024-03-30 13:02:23 +0100

    added metadata to principal page

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -6,8 +6,19 @@ import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 export const metadata: Metadata = {
+   metadataBase: new URL(process.env.PATHNAME || 'http://localhost:3000'),
    title: 'Pizzeria Albatros',
    description: 'La migliore pizzeria di Francavilla al mare',
+   keywords:
+      'pizza, pizzeria, pizzeria di Francavilla al mare, pizzeria Italia, pizzeria Italiana, pizzeria al forno, pizzeria al forno di Francavilla al mare',
+   openGraph: {
+      title: 'Pizzeria Albatros',
+      description: 'La migliore pizzeria di Francavilla al mare',
+      phoneNumbers: ['+39 320 123 4567'],
+      url: process.env.PATHNAME || '',
+      emails: ['aLQ1M@example.com'],
+      images: [`/images/OG-principalPage.png`],
+   },
 }
 
 export default function RootLayout({
